73338b3c9516fa84b9d42542abc5b58753d03984
ben-on <biniyamalemu595@gmail.com>
2021-12-30 22:43:39 +0300

    second

---
+++ FIZZ BUZZ.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def fizzBuzz(self, n):
+        mylist=[]
+        for i in range(1,n+1):
+            if i%3==0 and i%5==0:
+                mylist.append("FizzBuzz")
+            elif i%3==0 :
+                mylist.append("Fizz")
+            elif i%5==0:
+                mylist.append("Buzz")
+            else :
+                mylist.append(str(i))
+            
+        return mylist
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        
