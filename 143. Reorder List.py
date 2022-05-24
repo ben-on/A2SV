c06c877d2a25a43fffb338585e04d98f6ae9dfec
BENIYAM-ALEMU <91593613+ben-on@users.noreply.github.com>
2022-05-24 14:16:00 +0300

    leetcode medium

--- 143. Reorder List.py
+++ 143. Reorder List.py
@@ -8,11 +8,12 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        root1,root2,root3,c1,c2,c3,lst1,lst2=head,head,head,0,0,0,[],[]
-        
+        root1,root2=head,head
+        c1,c2=0,0
         while root1 :
             c1+=1
             root1=root1.next
+        lst1,lst2=[],[]
         while root2 :
             c2+=1
             if c2 > c1//2 :
@@ -23,6 +24,9 @@ class Solution:
         lst1.reverse()
         if lst1 :
             lst1.pop()
+        
+        root3=head
+        c3=1
         while lst1 or lst2 :
             if c3%2==0 and lst1 :
                 root3.next=lst1.pop()
