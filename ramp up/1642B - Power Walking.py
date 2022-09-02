f0ce589cb5957539338a581c45441b0b1c8947f8
BENIYAM-ALEMU <91593613+ben-on@users.noreply.github.com>
2022-09-02 23:43:09 +0300

    leetcode medium

---
+++ ramp up/1642B - Power Walking.py
@@ -0,0 +1,22 @@
+t=int(input())
+for r in range(t):
+    n=int(input())
+    lst=input().split()
+    var=len(set(lst))
+    for k in range(1,n+1):
+        if k <= var:
+            print(var,end=" ")
+        else:
+            print(k,end=" ")
+    print()
+            
+        
+    
+
+                
+        
+    
+    
+    
+    
+    
