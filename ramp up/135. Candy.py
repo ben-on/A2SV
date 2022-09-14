c4b8146f19f8c0d64e63a35a658be6c30f1eca3c
BENIYAM-ALEMU <91593613+ben-on@users.noreply.github.com>
2022-09-14 21:45:49 +0300

    leetcode hard

---
+++ ramp up/135. Candy.py
@@ -0,0 +1,27 @@
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        n = len(ratings)
+        candy = n
+        i=1
+        while i<n :
+            if ratings[i] == ratings[i-1] :
+                i+=1
+                continue
+            peak = 0
+            while ratings[i] > ratings [i-1] :
+                peak+=1
+                candy += peak
+                i+=1
+                if i == n : 
+                    return candy
+            valley = 0
+            while i<n and ratings[i] < ratings[i-1]:
+                valley+=1
+                candy += valley
+                i+=1
+            candy -= min(peak, valley)
+        return candy
+            
+                    
+                
+            
